22c5d7acbab6dd883a3bbc3279a6ea46c7727635
ChrisGardian <44732253+ChrisGardian@users.noreply.github.com>
2018-11-27 16:36:28 +0100

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 14:58:07 2018
+
+@author: lchris
+"""
+x=3
+a=0
+b=100
+i=0
+primes=[2]
+lenght=len(primes)
+for x in range(a,b):
+    while i<=lenght:
+       s=primes[i]
+       if (x%s==0):
+           primes.append(x)
+           if x in range(a,b):
+               print(x)
+    i=i+1
+x=x+1
+               
